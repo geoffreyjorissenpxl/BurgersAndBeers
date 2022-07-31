6415f42edd284cf589d827718c1c03c145b76a8d
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2022-08-01 00:48:50 +0200

    Edit: fixed layout issues + added screen responsiveness

--- src/Components/main-header/main-header.js
+++ src/Components/main-header/main-header.js
@@ -1,16 +1,16 @@
-import './Main-header.css';
-import Nav from '../nav/nav';
-import FoodCard from '../food-card/food-card';
+import "./Main-header.css";
+import Nav from "../nav/nav";
+import FoodCard from "../food-card/food-card";
 
 const MainHeader = () => {
   return (
-      <header>
-      <Nav/>
+    <div className="header">
+      <Nav />
         <div className="title">Welcome To Burgers And Beers</div>
         <a className="scroll-btn" href="#menu">
           <p>Click To</p> <p>Scroll Down</p>
         </a>
-      </header>
+    </div>
   );
 };
 
