36b5088222519bae0ccb46d2277ba77f6ad34375
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2022-08-05 02:34:27 +0200

    Add: implemented navigation

--- src/Components/nav/nav.js
+++ src/Components/nav/nav.js
@@ -7,8 +7,8 @@ const Nav = () => {
           <div className="nav-container">
             <div className="logo">Logo</div>
             <ul className="nav">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Menu</a></li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/menu">Menu</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
           </div>
