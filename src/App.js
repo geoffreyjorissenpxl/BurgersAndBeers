cb5a3e291fb196fc4cbcdbc2afb507abc0cda776
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2022-08-05 02:36:17 +0200

    Add: implemented axios for api calls fetching data for menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router , Routes, Route  } from 'react-router-dom';
+import { useEffect, useState, useMemo } from 'react';
 import Nav from './Components/nav/nav';
 import MainHeader from './Components/main-header/main-header';
 import MainPage from './main-page/mainpage';
+import MenuPage from './menu-page/menupage';
+import axios from 'axios';
 
 function App() {
+
+  const [allSodas, setAllSodas] = useState([]);
+  const [allBurgers, setAllBurgers] = useState([]);
+  const [allVeggies, setAllveggies] = useState([]);
+  const [allBeers, setAllBeers] = useState([]);
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("http://localhost:3004/products")
+      .then(res => {
+        setAllSodas(res.data['sodas']);
+        setAllBeers(res.data['beers']);
+        setAllBurgers(res.data['burgers']);
+        setAllveggies(res.data['veggies']);
+      });
+    };
+    fetchData();
+
+  }, []);
+
   return (
-    <MainPage/>
-    
+    <Router>
+    <div>
+      <Routes>
+      <Route path='/menu' element={<MenuPage sodas={allSodas} beers={allBeers} burgers={allBurgers} veggies={allVeggies} />} />
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </div>
+  </Router>
   );
 }
 
